249233c14694e40093189235fa5145e172595160
reza seyedi <reza.seyedi010@gmail.com>
2025-03-13 08:26:41 +0330

    deploying

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', async (req, res) => {
     try {
         
-        res.send('test')
+        res.send('retab server is working fine.')
         // const result = await testModule();
         // res.send(result)
 
